ade2fb9089bd3e4ed52de5ff9b37c33fcf2154d2
Diogo Cipriano <ext.diogo.cipriano@inseinc.com>
2023-05-22 00:12:03 +0100

    Health controller

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    private HealthController healthController;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        healthController = GetComponent<HealthController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void TakeDamage(float damage)
+    {
+        healthController.TakeDamage(damage);
+    }
+}
