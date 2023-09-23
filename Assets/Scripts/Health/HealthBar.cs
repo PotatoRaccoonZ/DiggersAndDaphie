8a93d742664afb0bf999569c7dedb5e770e49920
Diogo Cipriano <ext.diogo.cipriano@inseinc.com>
2023-09-23 22:47:50 +0100

    updates

--- Assets/Scripts/Health/HealthBar.cs
+++ Assets/Scripts/Health/HealthBar.cs
@@ -6,20 +6,23 @@ using UnityEngine.UI;
 public class HealthBar : MonoBehaviour
 {
     [SerializeField] private Image healthBar;
-    // Start is called before the first frame update
-    void Start()
+    private float reducedSpeed = 2;
+    private float target = 1;
+    private Camera cam;
+
+    private void Start()
     {
-        
+        cam = Camera.main;
     }
 
-    // Update is called once per frame
     void Update()
     {
-
+        transform.rotation = Quaternion.LookRotation(transform.position - cam.transform.position);
+        healthBar.fillAmount = Mathf.MoveTowards(healthBar.fillAmount, target, reducedSpeed * Time.deltaTime);
     }
 
     public void UpdateHealthBar(float currentHealth, float maxHealth)
     {
-        healthBar.fillAmount = currentHealth / maxHealth;
+        target = currentHealth / maxHealth;
     }
 }
