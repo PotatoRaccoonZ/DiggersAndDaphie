4dc022dc766aac9099b068af158a512451584c43
Diogo Cipriano <ext.diogo.cipriano@inseinc.com>
2023-09-27 18:51:10 +0100

    enemy prefab and animations

--- Assets/Scripts/Health/HealthController.cs
+++ Assets/Scripts/Health/HealthController.cs
@@ -3,63 +3,55 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class HealthController : MonoBehaviour
-{
+public class HealthController: MonoBehaviour {
     [SerializeField] private float currentHealth;
     [SerializeField] private float maxHealth = 10f;
     private HealthBar healthBar;
 
     // Start is called before the first frame update
-    void Start()
-    {
+    void Start() {
         currentHealth = maxHealth;
         healthBar = GetComponentInChildren<HealthBar>();
     }
 
     // Update is called once per frame
-    void Update()
-    {
+    void Update() {
 
     }
 
-    public void Heal(float health)
-    {
+    public void Heal( float health ) {
         currentHealth += health;
-        healthBar.UpdateHealthBar(currentHealth, maxHealth);
+        healthBar.UpdateHealthBar( currentHealth, maxHealth );
 
-        if (currentHealth > maxHealth)
-        {
+        if ( currentHealth > maxHealth ) {
             currentHealth = maxHealth;
         }
     }
 
-    public void TakeDamage(float damage)
-    {
+    public void TakeDamage( float damage ) {
         currentHealth -= damage;
-        healthBar.UpdateHealthBar(currentHealth, maxHealth);
+        healthBar.UpdateHealthBar( currentHealth, maxHealth );
 
-        if (currentHealth <= 0)
-        {
+        if ( currentHealth <= 0 ) {
             Die();
         }
     }
 
-    public void Die()
-    {
+    public void Die() {
         /**
          * TODO
          * - enemy on death behaviour -> currently destroying object (add animations in the future
          * - player on death behaviour
          */
-        if (!gameObject.CompareTag("Player"))
-        {
-            Destroy(gameObject);
+        if ( CompareTag( "Enemy" ) ) {
+            EnemyController enemyController = GetComponent<EnemyController>();
+            enemyController.Die();
+            Destroy( gameObject );
         }
-        Debug.Log("DEAD");
+        Debug.Log( "DEAD" );
     }
 
-    public float getCurrentHealth()
-    {
+    public float getCurrentHealth() {
         return currentHealth;
     }
 }
--- Assets/Scripts/SwordController.cs
+++ Assets/Scripts/SwordController.cs
@@ -2,26 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SwordController : MonoBehaviour
-{
+public class SwordController: MonoBehaviour {
     [SerializeField] private float damage = 1f;
     private Collider swordCollider;
 
     // Start is called before the first frame update
-    void Start()
-    {
+    void Start() {
         swordCollider = GetComponent<Collider>();
         // currently we only activate the collider when the player presses to attack
         // will eventually have to find a better way to do this most likely (if necessary)
         swordCollider.enabled = false;
     }
 
-    private void OnTriggerEnter(Collider other)
-    {
-        if (other.CompareTag("Enemy"))
-        {
-            other.GetComponentInParent<HealthController>().TakeDamage(damage);
-            Debug.Log("Enemy Health: " + other.GetComponentInParent<HealthController>().getCurrentHealth());
+    private void OnTriggerEnter( Collider other ) {
+        if ( other.CompareTag( "Enemy" ) ) {
+            other.GetComponentInParent<EnemyController>().TakeDamage( damage );
+            Debug.Log( "Enemy Health: " + other.GetComponentInParent<HealthController>().getCurrentHealth() );
             // I will eventually find another and better way to do this thing below (if necessary)
             // this is just to prevent multiple hits in a single swing
             swordCollider.enabled = false;
