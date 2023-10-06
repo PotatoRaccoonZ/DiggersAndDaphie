3e7365dd751ad0ef4abdbb40ce846f656e781df0
Daphiee <79719221+Daphiee@users.noreply.github.com>
2023-10-06 15:36:08 +0100

    Tower Projectiles Deal Damage

--- Assets/Scripts/Projectiles/ProjectileController.cs
+++ Assets/Scripts/Projectiles/ProjectileController.cs
@@ -1,10 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEditor;
 using UnityEngine;
 
 public class ProjectileController: MonoBehaviour {
     [SerializeField] private float _speed = 25.0f;
+    [SerializeField] private float damage = 1.0f;
+    [SerializeField] private Collider bulletCollider;
     private Rigidbody _rb;
     private Transform target;
 
@@ -13,6 +16,16 @@ public class ProjectileController: MonoBehaviour {
     }
     // Start is called before the first frame update
     void Start() {
+        bulletCollider = GetComponent<Collider>();
+    }
+    private void OnTriggerEnter( Collider other ) {
+        if ( other.CompareTag( "Enemy" ) ) {
+            other.GetComponentInParent<EnemyController>().TakeDamage( damage );
+            Debug.Log( "Enemy Health: " + other.GetComponentInParent<HealthController>().getCurrentHealth() );
+            Destroy(gameObject);
+            // I will eventually find another and better way to do this thing below (if necessary)
+            // this is just to prevent multiple hits in a single swing
+        }
     }
 
     void Update(){
@@ -23,12 +36,13 @@ public class ProjectileController: MonoBehaviour {
         Vector3 dir = target.position - transform.position;
         float distanceThisFrame = _speed * Time.deltaTime;
 
-        if(dir.magnitude <= distanceThisFrame){
+        /**if(dir.magnitude <= distanceThisFrame){
             Destroy(gameObject);
             Debug.Log("Hit Target!");
             return;
-        }
-
+        }**/
         transform.Translate(dir.normalized * distanceThisFrame, Space.World);
     }
+
+
 }
